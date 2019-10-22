853350a9684190eb146738f5f8d3fe63449aa19e
Antoine Reveau <null>
2019-10-22 17:53:47 +0200

    basic component with api call

---
+++ src/components/todos.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Todos = () => {
+  return(
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+export default Todos;
